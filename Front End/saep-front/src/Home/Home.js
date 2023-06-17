ff5a336032e776060b37676aacd2cad4aa9ecaef
João Henrique <joaohnsousa@gmail.com>
2023-06-17 18:21:18 -0300

    Etapa 2

--- Front End/saep-front/src/Home/Home.js
+++ Front End/saep-front/src/Home/Home.js
@@ -1,20 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import './Home.css'
 
 const Home = () => {
   return (
     <div>
-      <div className="flex items-center justify-center h-screen">
+      <div className="box flex items-center justify-center h-screen">
         <div>
           <h1 className="flex items-center justify-center mb-10 text-4xl font-bold">
             Áreas
           </h1>
           <div className="flex flex-wrap justify-center gap-4">
             {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((id) => {
-              let className = `w-16 h-16 bg-red-500 rounded-lg mx-2 flex items-center justify-center text-white text-2xl font-bold`;
+              let className = `quadrado w-16 h-16 rounded-lg mx-2 flex items-center justify-center text-dark text-2xl font-bold`;
 
               if (id === 3 || id === 5 || id === 6 || id === 11) {
-                className = `w-16 h-16 bg-neutral-300 rounded-lg mx-2 flex items-center justify-center text-red text-2xl font-bold`;
+                className = `quadCinza w-16 h-16 rounded-lg mx-2 flex items-center justify-center text-dark text-2xl font-bold`;
               }
 
               return (
--- Front End/saep-front/src/Modal/Modal.js
+++ Front End/saep-front/src/Modal/Modal.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import {BiHomeAlt} from 'react-icons/bi'
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { BiHomeAlt } from "react-icons/bi";
+import './Modal.css'
 
 const Modal = () => {
-const {id} = useParams();
-const [tableData, setTableData] = useState([]);
-
+  const { id } = useParams();
+  const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
     fetchData(id);
@@ -17,50 +17,65 @@ const [tableData, setTableData] = useState([]);
       const data = await response.json();
       setTableData(data);
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
   };
 
-
-
-    
   return (
     <div>
-        <div className="flex flex-col items-center justify-center h-screen bg-gray-200">
-      <div className="bg-white p-10 rounded-lg shadow-lg">
-        <Link to={`/`}>
-            <h1 className="text-3xl font-bold mb-4"><BiHomeAlt/></h1>
-        </Link>
-        <h1 className="flex flex-col text-3xl font-bold mb-4 content-center items-center">Área {id}</h1>
-        {tableData.length == 0 ? <h1>Não possui automoveis nessa área</h1> : <table className="w-full border border-gray-300">
-          <thead>
-            <tr>
-              <th className="border border-gray-300 px-4 py-2">Modelo</th>
-              <th className="border border-gray-300 px-4 py-2">Preço</th>
-              <th className="border border-gray-300 px-4 py-2">Vender</th>
-            </tr>
-          </thead>
-          <tbody>
-          {tableData.map((item) => (
-            <tr
-            key={id}>
-            <td className="border border-gray-300 px-4 py-2">{item.modelo}</td>
-              <td className="border border-gray-300 px-4 py-2">{item.preco}</td>
-              <td className="border border-gray-300 px-4 py-2">
-                <Link to={`/concessionaria/${item.modelo}/${item.id}/${item.area}`}>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                  Vender
-                </button>
-                </Link>
-              </td>
-          </tr>
-        ))}
-          </tbody>
-        </table> }
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-200">
+        <div className="bg-white p-10 rounded-lg shadow-lg">
+          <Link to={`/`}>
+            <h1 className="text-3xl font-bold mb-4">
+              <BiHomeAlt />
+            </h1>
+          </Link>
+          <h1 className="flex flex-col text-3xl font-bold mb-4 content-center items-center">
+            Área {id}
+          </h1>
+          {tableData.length == 0 ? (
+            <h1 className="text-2xl font-bold text-gray-800">
+              Não Possui Automoveis Nessa Área
+            </h1>
+          ) : (
+            <table className="w-full border border-gray-300">
+              <thead>
+                <tr>
+                  <th className="border border-gray-300 px-4 py-2">Modelo</th>
+                  <th className="border border-gray-300 px-4 py-2">Preço</th>
+                  <th className="border border-gray-300 px-4 py-2">Qntd</th>
+                  <th className="border border-gray-300 px-4 py-2">Vender</th>
+                </tr>
+              </thead>
+              <tbody>
+                {tableData.map((item) => (
+                  <tr key={id}>
+                    <td className="border border-gray-300 px-4 py-2">
+                      {item.modelo}
+                    </td>
+                    <td className="border border-gray-300 px-4 py-2">
+                      {item.preco}
+                    </td>
+                    <td className="border border-gray-300 px-4 py-2 text-center">
+                      {item.quantidade}
+                    </td>
+                    <td className="border border-gray-300 px-4 py-2 ">
+                      <Link
+                        to={`/concessionaria/${item.modelo}/${item.id}/${item.area}`}
+                      >
+                        <button className="botao hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                          Vender
+                        </button>
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </div>
       </div>
     </div>
-    </div>
-    
   );
 };
 
